39763dac4c3fc275559bdcdd4a20f0023b962dd0
Andres <31314529+Andres1097@users.noreply.github.com>
2025-07-10 13:09:34 -0300

    Se agrego DisponibilidadRestController y update a DisponibilidadDTO

--- src/main/java/com/Gropo06/turnos_medicos/dto/DisponibilidadDTO.java
+++ src/main/java/com/Gropo06/turnos_medicos/dto/DisponibilidadDTO.java
@@ -3,22 +3,33 @@ package com.Gropo06.turnos_medicos.dto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import com.Gropo06.turnos_medicos.model.enums.DiaSemana;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class DisponibilidadDTO {
 	private Long idDisponibilidad;
+	
+	@JsonIgnore
 	private LocalDate fecha;
+	
+	@Schema(description = "Día de la semana", example = "LUNES")
 	private DiaSemana diaSemana;
 
-	@DateTimeFormat(pattern = "HH:mm")
+	@Schema(description = "Hora de inicio (formato HH:mm)", example = "08:00")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
 	private LocalTime horaInicio;
-	@DateTimeFormat(pattern = "HH:mm")
+	
+	@Schema(description = "Hora de fin (formato HH:mm)", example = "16:00")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
 	private LocalTime horaFin;
 
 	private Long profesionalId;
 	private Long sucursalId;
+	
+	@JsonIgnore
 	private String sucursalNombre;
 
 	public Long getIdDisponibilidad() {
